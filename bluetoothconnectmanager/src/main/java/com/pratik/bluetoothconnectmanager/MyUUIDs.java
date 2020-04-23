1e9d275f1d0c361693ab95e1365a7a1db568cbcf
Pratik Byathnal <pratikbyathnal@gmail.com>
2020-04-24 02:46:35 +0530

    Minor code changes Add licensing

--- bluetoothconnectmanager/src/main/java/com/pratik/bluetoothconnectmanager/MyUUIDs.java
+++ bluetoothconnectmanager/src/main/java/com/pratik/bluetoothconnectmanager/MyUUIDs.java
@@ -1,6 +1,24 @@
 package com.pratik.bluetoothconnectmanager;
 
-public class MyUUIDs {
+/**
+ * Created by thecoderpb on 24/04/20.
+ */
 
-    public static String getUUIDs(){ return "00001000-0000-1000-8000-00805f9b34fb"; }
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class MyUUIDs {
+
+    static String getUUIDs(){ return "00001000-0000-1000-8000-00805f9b34fb"; }
 }
